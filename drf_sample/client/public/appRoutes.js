c0e540f4f99fb67be9f9d95124756a9ae95ff029
skahuma <skahuma@github.com>
2017-02-17 12:02:33 +0300

    test

---
+++ drf_sample/client/public/appRoutes.js
@@ -0,0 +1,13 @@
+angular
+    .module('appRoutes', ["ui.router"])
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider.state({
+        name: 'retail',
+        url: '/',
+        templateUrl: 'public/components/retail/templates/retail.template',
+        controller: 'RetailController'
+    });
+
+    $urlRouterProvider.otherwise('/');
+}]);
